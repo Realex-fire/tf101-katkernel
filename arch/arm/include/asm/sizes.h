a26464ad5053e169a7cd402a72ae88cc61763776
realex-fire <goy2010@yandex.ru>
2013-11-20 18:19:26 +0800

    sizes: fix syntax

--- arch/arm/include/asm/sizes.h
+++ arch/arm/include/asm/sizes.h
@@ -52,7 +52,7 @@
 #define SZ_48M				0x03000000
 #define SZ_64M				0x04000000
 #define SZ_128M			0x08000000
-#define SZ_160M			(SZ_128M | SZ_32M)
+#define SZ_160M			0x0A000000
 #define SZ_170M			0x0AA00000
 #define SZ_256M			0x10000000
 #define SZ_512M			0x20000000
