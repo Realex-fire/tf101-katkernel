7e6d813443fb851039183ec54b2c634f558c4b66
realex-fire <goy2010@yandex.ru>
2013-11-20 18:14:42 +0800

    misc: fix stability and syntax error

--- arch/arm/include/asm/sizes.h
+++ arch/arm/include/asm/sizes.h
@@ -18,7 +18,7 @@
  */
 
 #ifndef __sizes_h
-#define __sizes_h                       1
+#define __sizes_h			1
 
 /* handy sizes */
 #define SZ_1				0x00000001
@@ -32,32 +32,33 @@
 #define SZ_256				0x00000100
 #define SZ_512				0x00000200
 
-#define SZ_1K                           0x00000400
-#define SZ_2K                           0x00000800
-#define SZ_4K                           0x00001000
-#define SZ_8K                           0x00002000
-#define SZ_16K                          0x00004000
-#define SZ_32K                          0x00008000
-#define SZ_64K                          0x00010000
-#define SZ_128K                         0x00020000
-#define SZ_256K                         0x00040000
-#define SZ_512K                         0x00080000
+#define SZ_1K				0x00000400
+#define SZ_2K				0x00000800
+#define SZ_4K				0x00001000
+#define SZ_8K				0x00002000
+#define SZ_16K				0x00004000
+#define SZ_32K				0x00008000
+#define SZ_64K				0x00010000
+#define SZ_128K			0x00020000
+#define SZ_256K			0x00040000
+#define SZ_512K			0x00080000
 
-#define SZ_1M                           0x00100000
-#define SZ_2M                           0x00200000
-#define SZ_4M                           0x00400000
-#define SZ_8M                           0x00800000
-#define SZ_16M                          0x01000000
-#define SZ_32M                          0x02000000
-#define SZ_48M                          0x03000000
-#define SZ_64M                          0x04000000
-#define SZ_128M                         0x08000000
-#define SZ_160M                         (SZ_128M | SZ_32M)
-#define SZ_256M                         0x10000000
-#define SZ_512M                         0x20000000
+#define SZ_1M				0x00100000
+#define SZ_2M				0x00200000
+#define SZ_4M				0x00400000
+#define SZ_8M				0x00800000
+#define SZ_16M				0x01000000
+#define SZ_32M				0x02000000
+#define SZ_48M				0x03000000
+#define SZ_64M				0x04000000
+#define SZ_128M			0x08000000
+#define SZ_160M			(SZ_128M | SZ_32M)
+#define SZ_170M			0x0AA00000
+#define SZ_256M			0x10000000
+#define SZ_512M			0x20000000
 
-#define SZ_1G                           0x40000000
-#define SZ_2G                           0x80000000
+#define SZ_1G				0x40000000
+#define SZ_2G				0x80000000
 
 #endif
 
